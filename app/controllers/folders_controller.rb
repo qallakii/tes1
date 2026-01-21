5afeb2b192b2802e1fc97bafd10d1edcba6b6125
Admin <admin@MacBookPro.bbrouter>
2026-01-21 14:33:48 +0100

    Fix folder rename modal (correct route + CSRF-safe)

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -1,6 +1,6 @@
 class FoldersController < ApplicationController
   before_action :require_login
-  before_action :set_folder, only: %i[show destroy rename]
+  before_action :set_folder, only: %i[show destroy update]
 
   def index
     @folders = current_user.folders.where(parent_id: nil).order(updated_at: :desc)
@@ -9,9 +9,6 @@ class FoldersController < ApplicationController
   def show
     @subfolders = @folder.subfolders.order(updated_at: :desc)
     @cvs = @folder.cvs.includes(file_attachment: :blob).order(updated_at: :desc)
-
-    # ✅ used for Move Tree modal
-    @all_folders_for_tree = current_user.folders.select(:id, :name, :parent_id).order(:name)
   end
 
   def new
@@ -29,12 +26,23 @@ class FoldersController < ApplicationController
     end
   end
 
-  # ✅ rename from kebab menu (modal)
-  def rename
-    if @folder.update(params.require(:folder).permit(:name))
+  # Keep normal update if you use it elsewhere
+  def update
+    if @folder.update(folder_params.slice(:name))
       redirect_back fallback_location: folder_path(@folder), notice: "Folder renamed."
     else
       redirect_back fallback_location: folder_path(@folder), alert: @folder.errors.full_messages.to_sentence
+    end
+  end
+
+  # ✅ NEW: rename endpoint that never changes form action
+  def rename
+    folder = current_user.folders.find(params[:id])
+
+    if folder.update(params.require(:folder).permit(:name))
+      redirect_back fallback_location: folder_path(folder), notice: "Folder renamed."
+    else
+      redirect_back fallback_location: folder_path(folder), alert: folder.errors.full_messages.to_sentence
     end
   end
 
